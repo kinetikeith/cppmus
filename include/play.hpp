9dc5ea5e54a9045d3c742595201b40c413916f4b
kinetikeith <68295759+kinetikeith@users.noreply.github.com>
2020-12-05 16:46:32 -0500

    Updated play() function signature, added PlayInfo

--- include/play.hpp
+++ include/play.hpp
@@ -1,11 +1,23 @@
 #ifndef PLAY_HPP
 #define PLAY_HPP
 
-#include "Song.hpp"
-
 #include "portaudio.h"
 
+#include "PlaybackContext.hpp"
+#include "FrameInfo.hpp"
+#include "BufferInfo.hpp"
+
+struct PlayInfo
+{
+
+	PlaybackContextPtr	playbackContext;
+	FrameInfo			frameInfo;
+	BufferInfo			bufferInfo;
+
+};
+
 int audioCallback(const void*, void*, unsigned long, PaStreamCallbackTimeInfo, PaStreamCallbackFlags, void*);
-PaError play(Song*, unsigned int);
+
+PaError play(PlaybackContextPtr, size_t);
 
 #endif /* PLAY_HPP */
