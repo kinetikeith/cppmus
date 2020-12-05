5ae49c30e122cb79597fe2df6fde932e55deef98
kinetikeith <68295759+kinetikeith@users.noreply.github.com>
2020-12-05 15:45:58 -0500

    added AudioInputPtr

--- include/AudioInput.hpp
+++ include/AudioInput.hpp
@@ -1,9 +1,12 @@
 #ifndef AUDIOINPUT_HPP
 #define AUDIOINPUT_HPP
 
+#include <memory>
+
 #include "types.hpp"
 #include "BufferInput.hpp"
 
 using AudioInput = BufferInput<audio_t>;
+using AudioInputPtr = std::shared_ptr<AudioInput>;
 
 #endif /* AUDIOINPUT_HPP */
