01ae2985a02408c2a7949c5855cab9ebc7c38206
kinetikeith <68295759+kinetikeith@users.noreply.github.com>
2020-12-28 23:04:18 -0500

    update BufferIterator.hpp

--- include/BufferIterator.hpp
+++ include/BufferIterator.hpp
@@ -13,12 +13,12 @@ class BufferIterator : public std::iterator<std::random_access_iterator_tag, T, 
 {
 public:
 
-				BufferIterator(const BufferInfo&, const FrameInfo&);
+				BufferIterator(const Buffer<T>&, const BufferInfo&, const FrameInfo&);
 				BufferIterator(const BufferIterator&);
 	
 	T& operator*() const;
 	T& operator[](const size_t&) const;
-	T& get(size_t, size_t, size_t, bool) const;
+	T& get(const size_t&, const size_t&, const size_t&, const bool&) const;
 	
 	const BufferIterator& operator++();
 	const BufferIterator& operator--();
@@ -55,9 +55,39 @@ private:
 	size_t currentSampleIndex;
 	size_t currentVoiceIndex;
 	
-	const BufferInfo& bufferInfo;
-	const FrameInfo& FrameInfo;
+	Buffer<T>& buffer;
+	BufferInfo& bufferInfo;
+	FrameInfo& frameInfo;
 
 };
+
+template<typename T>
+BufferIterator<T>::BufferIterator(const Buffer<T>& buf,
+	const BufferInfo& bInfo, const FrameInfo& fInfo) : 
+		buffer(const_cast<Buffer<T>&>(buf)),
+		bufferInfo(const_cast<BufferInfo&>(bInfo)),
+		frameInfo(const_cast<FrameInfo&>(fInfo)),
+		currentIndex(0),
+		currentChannelIndex(0),
+		currentSampleIndex(0),
+		currentVoiceIndex(0)
+{
+	
+}
+
+template<typename T>
+BufferIterator<T>:: BufferInfo(const BufferIterator<T>& other) :
+	buffer(other.buffer),
+	bufferInfo(other.bufferInfo),
+	frameInfo(other.frameInfo),
+	currentIndex(other.currentIndex),
+	currentChannelIndex(other.currentChannelIndex),
+	currentSampleIndex(other.currentSampleIndex),
+	currentVoiceIndex(other.currentVoiceIndex)
+{
+	
+}
+
+
 
 #endif /* BUFFERITERATOR_HPP */
